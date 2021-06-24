417ea2ed081cd29c7cf731868d93d453846a5ec9
Kontam <tehayan0111@gmail.com>
2021-06-25 01:00:07 +0900

    add lhci/server

---
+++ src/server/index.ts
@@ -0,0 +1,11 @@
+import { createServer } from '@lhci/server';
+
+console.log('Starting server...');
+createServer({
+  port: process.env.PORT || 5000,
+  storage: {
+    storageMethod: 'sql',
+    sqlDialect: 'sqlite',
+    sqlDatabasePath: './db.sql',
+  },
+}).then(({ port }) => console.log('LHCI listening on port', port));
