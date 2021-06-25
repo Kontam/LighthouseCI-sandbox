4472b883d6c7835797dd77602803bd5dc90931b3
Kontam <tehayan0111@gmail.com>
2021-06-26 01:25:59 +0900

    feat: add upload config

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,11 @@ module.exports = {
       headful: false,
       url: 'https://konkonta.com',
     },
+    upload: {
+      target: 'lhci',
+      token: '9e7fb8dc-bb93-44a5-84a8-8e53547c7586',
+      serverBaseUrl: 'http://localhost:5000',
+    },
   },
   assert: {
     assertions: {
